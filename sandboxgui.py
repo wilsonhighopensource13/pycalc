49feb0077b446a83ce7e71d59e962fe98918759f
richiebful <richiebful@users.noreply.github.com>
2013-11-18 17:24:18 -0500

    update sandboxgui.py
    the original gui sandbox commit for the calculator. if editing please use multi-line quotes """ """ to keep the old version, even though it can be restored via command line.

--- sandboxgui.py
+++ sandboxgui.py
@@ -1,1 +1,19 @@
 #The GUI work is contained here.
+from Tkinter import *
+i = 0
+root = Tk()
+    
+frame = Frame(root, width=500, height=400)
+frame.config(bg="#838996",borderwidth=2)
+inputbox = Text(frame)
+outputbox = Text(frame)
+lbl=Label(frame ,text="Command Line")
+outputbox.grid(row=1,column=0)
+inputbox.grid(row=2,column=0)
+lbl.grid(row=0,column=0)
+outputbox.config()
+frame.pack()
+
+
+root.mainloop()
+
